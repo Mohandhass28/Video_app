bb7da37a7305a6cfa42b23f102d22c2734ed0d82
Kanagamani Dhass <Dhass15@users.noreply.github.com>
2024-04-13 19:21:39 +0530

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-[18px]">Index</Text>
+    </View>
+  );
+};
+
+export default index;
